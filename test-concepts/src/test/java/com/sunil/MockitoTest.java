3f9a843a8acba48c4d6b11b26ebf3297c2146d34
Sunil <sks.256@gmail.com>
2021-03-20 17:52:17 +0530

    Mockito

--- test-concepts/src/test/java/com/sunil/MockitoTest.java
+++ test-concepts/src/test/java/com/sunil/MockitoTest.java
@@ -1,8 +1,8 @@
 package com.sunil;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.*;
 public class MockitoTest {
 
     @Test
-    public void verifySomeBehavior(){
+    public void verifySomeBehavior() {
         //mock creation
         List<String> mockedList = mock(List.class);
 
@@ -24,7 +24,7 @@ public class MockitoTest {
     }
 
     @Test
-    public void someStabbing(){
+    public void someStabbing() {
 
         List<String> mockedList = mock(List.class);
 
@@ -32,7 +32,7 @@ public class MockitoTest {
         when(mockedList.get(1)).thenThrow(new IllegalArgumentException());
 
         System.out.println(mockedList.get(0));
-       // System.out.println(mockedList.get(1));
+        // System.out.println(mockedList.get(1));
         System.out.println(mockedList.get(2));
 
         //Assertions.assertThat(mockedList, doThrow(new IllegalArgumentException()));
@@ -42,4 +42,76 @@ public class MockitoTest {
         verify(mockedList).get(2);
 
     }
+
+
+    @Test
+    public void argumentMatcher1() {
+
+        // given
+        LinkedList<String> mockedLinkedList = mock(LinkedList.class);
+        when(mockedLinkedList.get(anyInt())).thenReturn("Sunil");
+        when(mockedLinkedList.contains(anyString())).thenReturn(true);
+
+        //when
+        System.out.println(mockedLinkedList.get(9999));
+        System.out.println(mockedLinkedList.contains("hello world"));
+
+        //then
+        verify(mockedLinkedList).get(anyInt());
+        verify(mockedLinkedList).contains(anyString());
+
+    }
+
+
+    interface Dry {
+        String dryRun(Boolean flag);
+    }
+
+    @Test
+    public void argumentMatcherForNull() {
+
+        Dry mock = mock(Dry.class);
+        // stubbing using anyBoolean() argument matcher
+        when(mock.dryRun(any(Boolean.class))).thenReturn("state");
+
+        // below the stub won't match, and won't return "state"
+        System.out.println(mock.dryRun(null));
+
+        // either change the stub
+        when(mock.dryRun(isNull())).thenReturn("state");
+        System.out.println(mock.dryRun(null)); // ok
+
+        // or fix the code ;)
+        when(mock.dryRun(anyBoolean())).thenReturn("state");
+        mock.dryRun(true); // ok
+    }
+
+    static class MyClass {
+
+        protected String method() {
+            throw new NullPointerException();
+        }
+    }
+
+    @Test
+    public void whenThenReturnVSdoReturnWhenCase1() {
+
+        MyClass myClass = spy(MyClass.class);
+        // would work fine
+        doReturn("test").when(myClass).method();
+        System.out.println(myClass.method());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenThenReturnVSdoReturnWhenCase2() {
+
+        MyClass myClass = spy(MyClass.class);
+
+        // would throw a NullPointerException
+        when(myClass.method()).thenReturn("test");
+        verify(myClass).method();
+
+    }
+
+
 }
